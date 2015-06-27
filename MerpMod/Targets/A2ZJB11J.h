c0593de57cc972a658ed67c41149b93ffefbd905
Scott Anderson <kerchakone@yahoo.com>
2015-06-27 13:21:34 -0700

    Target Updates for 05 STI

--- MerpMod/Targets/A2ZJB11J.h
+++ MerpMod/Targets/A2ZJB11J.h
@@ -1,7 +1,7 @@
 #define MOD_DATE 14.6.13.119
 
-#include "Flash.h"
-#define MOD_CONFIG Flash
+#include "Switch.h"
+#define MOD_CONFIG Switch
 #define MOD_BUILD Testing
 #define MOD_RELEASE 0
 #define ECU_CALIBRATION_ID A2ZJB11J
@@ -21,6 +21,9 @@
 // Switch Hacks
 /////////////////////
 
+//#define pTGVLeftVoltage ((unsigned short*)0xFFFF5C0A)
+//#define pTGVRightVoltage ((unsigned short*)0xFFFF5C0C)
+//#define sShortToFloat (0x00002594)
 
 /////////////////////
 // Rev Limit Hack
@@ -44,6 +47,8 @@
 /////////////////////
 
 #define dInjectorScaling ((float*)0x000C5BD0)
+//#define hInjectorScaling (0x0002B738)
+
 
 /////////////////////
 // Cel Hacks
@@ -57,38 +62,80 @@
 // Boost Hacks
 /////////////////////
 
+//#define hPullTargetBoost (0x00017750)
+//#define hTableTargetBoost (0x00017764)
+//#define tTargetBoost (0x000939C4)
+//#define hTableTargetBoostAlt (0x00017758)
+//#define tTargetBoostAlt (0x000939A8)
 
 /////////////////////
 // WGDC Hacks
 /////////////////////
 
+//#define hPullWgdc (0x00017BCC)
 #define hWgdc (0x00011D34)
 #define sWgdc (0x00013A8C)
+//#define sJsrWgdcInitial (0x00017AC2)
+//#define hTableWgdcInitial (0x00017BD4)
+//#define tWgdcInitial (0x0009391C)
+//#define tTableWgdcInitialAlt (0x00017BDC)
+//#define hJsrWgdcMax (0x00017B24)
+//#define hTableWgdcMax (0x00017C10)
 #define tWgdcMax (0x000788C0)
+//#define tWgdcMaxAlt (0x00093938)
 
 /////////////////////
 // Primary Open Loop Fueling Hacks
 /////////////////////
 
 #define pPolf4Byte (0xFFFF54E4)
+//#define hPull3DPolf (0x000306A0)
+//#define tPolf (0x00095420)
+//#define hTablePolf (0x000306AC)
+//#define hJsrPolf (0x00030518)
+//#define hPolf (0x00014EE0)
+//#define sPolf (0x0003046C)
 #define pPolfEnrich (0xFFFF54E4)
 
 /////////////////////
 // Timing Hacks
 /////////////////////
 
+//#define hBaseTiming (0x00014F9C)
+//#define hTableBaseTiming (0x00037DE0)
+//#define tBaseTiming (0x00095B28)
 #define pBaseTiming (0xFFFF57E0)
+//#define sBaseTiming (0x00037C72)
+//#define hPull3DTiming (0x00037DDC)
+//#define hSubKca (0x00037654)
 #define pKcaIam (0xFFFF5910)
+//#define pBaseTimingIdle (0xFFFF76C0)
+//#define hPullBaseTimingIdle (0x00038290)
+//#define hTableBaseTimingIdleInGearA (0x00038294)
+//#define tBaseTimingIdleInGearA (0x00095734)
+//#define hTableBaseTimingIdleInGearB (0x00038298)
+//#define tBaseTimingIdleInGearB (0x00095748)
+//#define hTableBaseTimingIdleNeutralA (0x0003829C)
+//#define tBaseTimingIdleNeutralA (0x0009575C)
+//#define hTableBaseTimingIdleNeutralB (0x000382A0)
+//#define tBaseTimingIdleNeutralB (0x00095770)
+
 
 /////////////////////
 // Spark Cut
 /////////////////////
 
+//#define sSparkCutOcrStart (0x0000A756)
+//#define sSparkCutOcrEnd (0x0000A76C)
+//#define sSparkCutGrStart (0x0000A7C6)
+//#define sSparkCutGrEnd (0x0000A7CE)
 
 /////////////////////
 // Flags-Signals
 /////////////////////
 
+//#define pCruiseMasterFlags ((unsigned char*)0xFFFF64B0)
+//#define CruiseMasterBitMask ((unsigned char)0x01)
 #define pResumeFlags ((unsigned char*)0xFFFF4C0C)
 #define ResumeBitMask ((unsigned char)0x04)
 #define pCoastFlags ((unsigned char*)0xFFFF4C0C)
@@ -97,12 +144,24 @@
 #define BrakeBitMask ((unsigned char)0x08)
 #define pClutchFlags ((unsigned char*)0xFFFF5039)
 #define ClutchBitMask ((unsigned char)0x80)
+//#define pIdleFlags ((unsigned char*)0xFFFF6946)
+//#define IdleBitMask ((unsigned char)0x80)
+//#define pNeutralFlags ((unsigned char*)0xFFFF69EF)
+//#define NeutralBitMask ((unsigned char)0x02)
+//#define pDefoggerFlags ((unsigned char*)0xFFFF69EF)
+//#define DefoggerBitMask ((unsigned char)0x20)
+//#define pClosedLoopFlags ((unsigned char*)0xFFFF9299)
+//8 = Closed Loop (normal)]  [10 = Open Loop (normal)]  [7 = Open Loop due insufficient coolant temp]  [14 = Open Loop due to system failure].
+
+
 
 /////////////////////
 // NonSpecific Engine params
 /////////////////////
 
+//#define pFbkc1 ((unsigned char*)0xFFFF6B6C)
 #define pFbkc4 ((float*)0xFFFF58BC)
+//#define pIam1 ((unsigned char*)0xFFFF6B71)
 #define pIam4 ((float*)0xFFFF2588)
 #define pEngineSpeed ((float*)0xFFFF5068)
 #define pVehicleSpeed ((float*)0xFFFF5058)
@@ -115,8 +174,81 @@
 #define pEngineLoad ((float*)0xFFFF4F48)
 #define pReqTorque ((float*)0xFFFF5AB4)
 #define pThrottlePlate ((float*)0xFFFF4E50)
+//#define pWgdc4 ((float*)0xFFFF63A0)
 #define pCurrentGear ((unsigned char*)0xFFFF515D)
 #define pAf1Res ((float*)0xFFFF4A48)
+//#define pDeltaMap ((float*)0xFFFF668C)
+//#define pAcceleratorPedal ((float*)0xFFFF68E4)
+//#define pThrottlePlateTarget ((float*)0xFFFF7AE8)
+//#define tTargetThrottlePlateA (0x00096498)
+//#define hTableTargetThrottlePlateA (0x000BE734)
+//#define pTargetIdleSpeed ((float*)0xFFFF7C3C)
+//#define hPullTargetIdleSpeed (0x00042EB8)
+//#define hTableTargetIdleSpeedA (0x00042EBC)
+//#define tTargetIdleSpeedA (0x00095F1C)
+//#define hTableTargetIdleSpeedB (0x00042EC4)
+//#define tTargetIdleSpeedB (0x00095F44)
+//#define hTableTargetIdleSpeedC (0x00042ECC)
+//#define tTargetIdleSpeedC (0x00095F6C)
+//#define hPullIdleMapSelection (0x00042EA4)
+//#define pIdleMapSelection (0xFFFF7CB4)
+//#define hPullRequestedTorqueA (0x000BEA7C)
+//#define tRequestedTorqueA (0x000964D0)
+//#define hTableRequestedTorqueA (0x000BE9FC)
+//#define sFloatToShort (0x00002564)
+//#define sFloatToByte (0x000025A4)
+//#define dEvapFuelComp (0x000273c0)              //fix
+//#define pCPCDutyRatio ((float*)0xFFFF6BC0)
+//#define pAirPump ((float*)0xFFFF62CC)
+//#define pFuelLevel ((float*)0xFFFF67F0)
+//#define pSTFTs1 ((float*)0xFFFF707C)
+//#define pLTFTsA ((float*)0xFFFF7238)
+//#define dLTFTLimitA (0x000C8568)  Ready for idatohew & Metahooks
+//#define dLTFTLimitB (0x000C856C)  Ready for idatohew & Metahooks
+//#define hLTFTLimitA1 (0x0002EE44)
+//#define hLTFTLimitB1 (0x0002EE40)
+//#define hLTFTLimitA2 (0x0002F50C)
+//#define hLTFTLimitB2 (0x0002F508)
+//#define pAVCS ((float*)0xFFFF8308)
+//#define hPullAVCS (0x00053984)
+//#define tAVCS (0x00096A28)
+//#define hTableAVCS (0x0005398C)
+//#define pO2SensorScaling ((float*)0xFFFF5F00)
+//#define hPullO2SensorScaling (0x0000BFD8)
+//#define tO2SensorScaling (0x00096600)
+//#define hTableO2SensorScaling (0x0000BFD4)
+//#define pAFSensor1Current ((float*)0xFFFF5F04)
+//#define hOpenLoopAFRmin (0x000306C0)
+//#define dOpenLoopAFRmin (0x000C8598)
+//#define dSSMReplace (0x0004E510)
+//#define hSSMReplace1 (0x00056290)
+//#define hSSMReplace2 (0x00056294)
+//#define hTargetTPSBandPass (0x000BE73C)
+//#define sBandPass (0x000024D8)
+//#define sDutyCycleOut (0x000023A8)
+//#define hDutyCycleOut (0x00011A00)
+//#define hTargetTPSIdle (0x000BEB2C)
+//#define pTargetTPSIdle ((float*)0xFFFF7B70)
+////#define sBasicMotions (0x0002495A)
+////#define hBasicMotions (0x0002495A)
+//#define hFuelCut (0x00035BD0)
+//#define pFuelCut ((short*)0xFFFF6F74)
+//#define pBoost ((float*)0xFFFF669C)
+//#define hPrepForSpark (0x00006E0C)
+//#define sPrepForSpark (0x0000A458)
+//#define pCyl1SparkCut ((unsigned char*)0xFFFF5DF0)
+//#define pCyl2SparkCut ((unsigned char*)0xFFFF5DF8)
+//#define pCyl3SparkCut ((unsigned char*)0xFFFF5E00)
+//#define pCyl4SparkCut ((unsigned char*)0xFFFF5E08)
+//#define hPedalSub (0x000067DC)
+//#define sPedalSub (0x0000CF24)
+//#define sAfterAD (0x00006e64)
+//#define sAfterReloadAD (0x000066b8)
+//#define hAfterAD (0x000067b4)
+//#define hAfterReloadAD (0x000066b8)
+//#define pPedalVoltage ((short*)0xFFFF5c00)
+//#define pSubPedalVoltage ((short*)0xFFFF5c02)
+//#define pTargetIdleAir ((float*)0xFFFF7b6c)
 
 /////////////////////
 // OBD Experimental stuff
@@ -140,4 +272,5 @@
 #define hMemoryReset (0x0000D1C0)
 #define pMemoryResetLimit (0xFFFFBFFB)
 #define hMemoryResetLimit (0x0000FE94)
+//#define sMemorySoftReset (0x00056A24)
 
